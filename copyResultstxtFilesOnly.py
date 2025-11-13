76c9d5389a6423c7648b686696b068c3d03dfa5a
Nolaraj Poudel <pnolaraj@gmail.com>
2025-11-13 20:39:04 +0545

    Update copyResultstxtFilesOnly.py

--- copyResultstxtFilesOnly.py
+++ copyResultstxtFilesOnly.py
@@ -36,51 +36,75 @@ def filePaths(Subfolders, FileName):
 
     return ResultFiles
 
+def Copier():
+    folderpath = open_folder_dialog()
+    subfolders = list_subfolders(folderpath)
+    FileName = "Result.txt"
+    ResultFiles = filePaths(subfolders, FileName)
 
+    print(len(ResultFiles))
+    validfiles = []
+    invalidfiles = []
+    for file in ResultFiles:
+        if os.path.getsize(file) > 1024:
+            validfiles.append(file)
+        else:
+            invalidfiles.append(file)
 
-folderpath = open_folder_dialog()
-subfolders = list_subfolders(folderpath)
-FileName = "Result.txt"
-ResultFiles = filePaths(subfolders, FileName)
+    Insidefolder = "TS_Outputs"
+    pathsContainer = {}
+    for index, file in enumerate(validfiles):
+        tspath = os.path.join(os.path.dirname(file), Insidefolder)
+        if not os.path.exists(tspath):
+            validfiles.remove(file)
+            invalidfiles.append(file)
+        else:
+            pathsContainer[index] = [tspath, file]
 
-print(len(ResultFiles))
-validfiles = []
-invalidfiles = []
-for file in ResultFiles:
-    if os.path.getsize(file) > 1024:
-        validfiles.append(file)
-    else:
-        invalidfiles.append(file)
+    # print(pathsContainer)
+    # print(len(validfiles), len(invalidfiles))
+    OutputDir = r"E:\Machine Learning Research\Numerical Analysis\ExtractedResults\From PremBhai\Extracted Results"
+    for key, value in pathsContainer.items():
+        TsFolder = value[0]
+        resultfile = value[1]
 
-Insidefolder = "TS_Outputs"
-pathsContainer = {}
-for index, file in enumerate(validfiles):
-    tspath = os.path.join(os.path.dirname(file), Insidefolder)
-    if not os.path.exists(tspath):
-        validfiles.remove(file)
-        invalidfiles.append(file)
-    else:
-        pathsContainer[index] = [tspath, file]
+        newTSpath = TsFolder.replace(folderpath, OutputDir, 1)
+        newResultPath = resultfile.replace(folderpath, OutputDir, 1)
+        if not os.path.exists(newTSpath):
+            shutil.copytree(TsFolder, newTSpath)
 
-# print(pathsContainer)
-# print(len(validfiles), len(invalidfiles))
+            newResultFolder = os.path.dirname(newTSpath)
+            shutil.copy2(resultfile, newResultFolder)
 
-#_________________________________________________________________________________________Output Defination
-OutputDir = r"E:\Machine Learning Research\Numerical Analysis\ExtractedResults\New folder"
-for key, value  in pathsContainer.items():
-    TsFolder = value[0]
-    resultfile = value[1]
+        print(newTSpath)
 
-    newTSpath = TsFolder.replace(folderpath, OutputDir, 1)
-    newResultPath  = resultfile.replace(folderpath, OutputDir, 1)
-    if not os.path.exists(newTSpath):
-        shutil.copytree(TsFolder, newTSpath)
+def findMissingResults():
+    Buildings = ["S", "L1", "L2", "L3", "L4", "R"]
+    ScaleFactor  = [0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1]
+    Soils = ["Fixed", "Soft", "Medium", "Hard"]
+    folderpath = open_folder_dialog()
+    pathUptoEq = []
+    for earthquakes in os.listdir(folderpath):
+        full_path = os.path.join(folderpath, earthquakes)
+        if os.path.isdir(full_path):  # ✅ only add if it's a folder
+            pathUptoEq.append(full_path)
+    
+    
+    missingDatapaths = []
+    for eqpath in pathUptoEq:
+        for sf in ScaleFactor:
+            for building in Buildings:
+                for soil in Soils:
+                    lastdir = os.path.join(eqpath, str(sf), building, soil)
+                    if not os.path.exists(lastdir):
+                        print(lastdir)
+                        missingDatapaths.append(lastdir)
+                    # print(lastdir)
 
-        newResultFolder = os.path.dirname(newTSpath)
-        shutil.copy2(resultfile, newResultFolder)
+    print(len(missingDatapaths))
 
-    print(newTSpath)
-    break
+# Copier()
+# findMissingResults
 
 
 
