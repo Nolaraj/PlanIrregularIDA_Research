3de0296c5d94c3ff264a7544974748b246f73a44
Nolaraj Poudel <pnolaraj@gmail.com>
2025-11-13 12:59:10 +0545

    Add script to copy Result.txt files from subfolders
    This script copies 'Result.txt' files from subfolders to a specified output directory, ensuring valid file sizes and existing target folders.

---
+++ copyResultstxtFilesOnly.py
@@ -0,0 +1,93 @@
+from openpyxl.styles import Font, Fill, Color, PatternFill  # Connect styles for text
+from openpyxl.utils import get_column_letter, column_index_from_string
+import openpyxl
+from openpyxl.utils.units import pixels_to_EMU
+from openpyxl.chart.text import RichText
+from openpyxl.drawing.text import Paragraph, ParagraphProperties, CharacterProperties, RichTextProperties, Font, RegularTextRun
+from openpyxl.drawing.fill import PatternFillProperties, ColorChoice
+
+import tkinter as tk
+from tkinter import filedialog
+import os
+import sqlite3
+import pandas as pd
+import shutil
+
+
+def open_folder_dialog():
+    root = tk.Tk()
+    root.withdraw()
+
+    folder_path = filedialog.askdirectory(
+        title="Select the root folder containing the results"
+    )
+
+    return folder_path
+def list_subfolders(root_folder):
+    subfolders = []
+
+    for dirpath, dirnames, filenames in os.walk(root_folder):
+        for dirname in dirnames:
+            subfolder_path = os.path.join(dirpath, dirname)
+            subfolders.append(subfolder_path)
+
+    return subfolders
+def filePaths(Subfolders, FileName):
+    ResultFiles = []
+    for subfolder in subfolders:
+        for files in os.listdir(subfolder):
+            if files.endswith(FileName):
+                Filepath = os.path.join(subfolder, files)
+                ResultFiles.append(Filepath)
+
+    return ResultFiles
+
+
+
+folderpath = open_folder_dialog()
+subfolders = list_subfolders(folderpath)
+FileName = "Result.txt"
+ResultFiles = filePaths(subfolders, FileName)
+
+print(len(ResultFiles))
+validfiles = []
+invalidfiles = []
+for file in ResultFiles:
+    if os.path.getsize(file) > 1024:
+        validfiles.append(file)
+    else:
+        invalidfiles.append(file)
+
+Insidefolder = "TS_Outputs"
+pathsContainer = {}
+for index, file in enumerate(validfiles):
+    tspath = os.path.join(os.path.dirname(file), Insidefolder)
+    if not os.path.exists(tspath):
+        validfiles.remove(file)
+        invalidfiles.append(file)
+    else:
+        pathsContainer[index] = [tspath, file]
+
+# print(pathsContainer)
+# print(len(validfiles), len(invalidfiles))
+OutputDir = r"E:\Machine Learning Research\Numerical Analysis\ExtractedResults\New folder"
+for key, value  in pathsContainer.items():
+    TsFolder = value[0]
+    resultfile = value[1]
+
+    newTSpath = os.path.join(OutputDir, )
+    newTSpath = TsFolder.replace(folderpath, OutputDir, 1)
+    newResultPath  = resultfile.replace(folderpath, OutputDir, 1)
+    if not os.path.exists(newTSpath):
+        shutil.copytree(TsFolder, newTSpath)
+
+        newResultFolder = os.path.dirname(newTSpath)
+        shutil.copy2(resultfile, newResultFolder)
+
+    print(newTSpath)
+    break
+
+
+
+
+
